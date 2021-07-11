607a3052a4830d3c6af94079c96c116058788f50
rabeeajahangir <69980221+rabeeajahangir@users.noreply.github.com>
2021-07-11 00:41:32 -0400

    added routes to get, add and delete employees

--- Routes/apiRoutes/employeeRoutes.js
+++ Routes/apiRoutes/employeeRoutes.js
@@ -19,4 +19,75 @@ router.get('/employees', (req, res) => {
       });
     });
   });
+
+  //GET SINGLE EMPLOYEE
+  router.get('/api/employees/:id', (req, res) => {
+    const sql = `SELECT * FROM employees WHERE id = ?`;
+    const params = [req.params.id];
   
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+  //DELETE AN EMPLOYEE
+  // Delete a candidate
+router.delete('/api/employees/:id', (req, res) => {
+    const sql = `DELETE FROM employees WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Employee not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+  
+  //CREATE AN EMPLOYEE
+  // Create a candidate
+router.post('/api/employees', ({ body }, res) => {
+    const errors = checkInput(body, 'first_name', 'last_name', 'job-title', 'departments', 'salary', 'manager_name');
+  
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+    const sql = `INSERT INTO candidates ('first_name', 'last_name', 'job-title', 'departments', 'salary', 'manager_name')
+  VALUES (?,?,?,?,?,?)`;
+const params = [body.first_name, body.last_name, body.job-title, body.departments, body.salary, body.manager_name];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'success',
+    data: body
+  });
+});
+  });
+
+
+
+
+
+  module.exports = router;  
